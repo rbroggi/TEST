4f91f2a8f22ab9587afb59ed6af031251639f8ac
JC1024 <mbp@iMac-3.local>
2013-11-04 16:23:49 +0100

    Prima prova

---
+++ file.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class B{
+	 public:
+	 std::string pub ;
+
+	B(string p1="PUB",string p2="PRI",string p3="PRO"):pub(p1),pri(p2),pro(p3){};
+
+	 std::string rpub(){return pub; };
+	 std::string rpri(){return pri; };
+	 std::string rpro(){return pro; };
+	 
+	 private:
+	 std::string pri ;	 
+	 protected:
+	 std::string pro ;
+};
+
+class D: public B{
+
+	public :
+	std::string diff;
+
+	 /*
+	 D(string p1="dPUB",string p2="dPRI",string p3="dPRO"):pub(p1),pri(p2),pro(p3){};
+
+	 std::string rpub(){return pub; };
+	 std::string rpri(){return pri; };
+	 std::string rpro(){return pro; };
+	 
+	 
+	 private:
+	 std::string pri ;	 
+	 protected:
+	 std::string pro ;
+	 */
+};
+int main() {
+	
+	D d;
+	
+	cout<<d.rpub();
+	cout<<d.rpri();
+	cout<<d.rpro();
+
+cout<<endl;
+	return 0;
+}
